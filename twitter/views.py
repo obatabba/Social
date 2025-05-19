f55190aca2685e35fbfd1cef866bddfafb1ffac6
Obada Tabbaa <obatabba@gmail.com>
2025-05-20 02:04:12 +0300

    Fix a bug where the whole registration form gets cleared when submiting invalid data.

--- twitter/views.py
+++ twitter/views.py
@@ -18,15 +18,15 @@ class CustomLogoutView(LogoutView):
     
 
 def register(request):
-    form = RegistrationForm()
-
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
-
+    else:
+        form = RegistrationForm()
+        
     return render(request, 'registration/register.html', {'form': form})
     
 
